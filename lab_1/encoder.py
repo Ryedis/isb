b5a9fd758c0755b81c8ab15fd2930515bf370cbd
Ryedis <134209766+Ryedis@users.noreply.github.com>
2024-04-10 14:15:18 +0400

    add frequency analysis

--- lab_1/encoder.py
+++ lab_1/encoder.py
@@ -1,5 +1,4 @@
 import argparse
-import logging
 import os
 import sys
 
@@ -7,18 +6,21 @@ import sys
 project_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(project_directory)
 
-from r_wr_funcs import read_txt, save_txt, save_json
+from utils.r_wr_funcs import read_txt, save_txt, save_json
 from utils.constants import ALPHABET
 
 
-def encode(input_file: str, output_file: str, output_json_file: str, shift: int):
+def encode(input_file: str, output_file: str, key_json_file: str, shift: int) -> None:
+    """
+    Encoding the text with the Caesar cipher
 
+    Args:
+        input_file: path .txt file
+        output_file: path output .txt file
+        key_json_file: path key .json file
+        shift: how much does the alphabet shift
+    """
     alf = ALPHABET["rus_alf"]
-
-    """
-    Encoding text from input_file with cesar's chipher with alphabet rotated by 'shift' times
-    """
-
     res: str = ""
 
     s = read_txt(input_file)
@@ -35,25 +37,25 @@ def encode(input_file: str, output_file: str, output_json_file: str, shift: int)
         res += encoded_dict.get(i, i)
 
     save_txt(output_file, res)
-    save_json(output_json_file, encoded_dict)
+    save_json(key_json_file, encoded_dict)
 
 
-if __name__ == "__main__":
+def main() -> None:
+    """
+    Uses argparse to process command-line arguments
+    and calls the encode() function to encode data.
+    """
     parser = argparse.ArgumentParser(description = encode.__doc__)
     parser.add_argument("input_file", type = str, help = "Input file name")
     parser.add_argument("output_file", type = str, help = "Output file name")
-    parser.add_argument("output_json_file", type = str, help = "Output key file name")
+    parser.add_argument("key_json_file", type = str, help = "Output key file name")
     parser.add_argument('shift', type = int, help = 'alphabet shift parametrization')
 
     args = parser.parse_args()
 
-    logging.basicConfig(
-    level=logging.DEBUG,
-    format='[{asctime}] #{levelname:8} {filename}:'
-           '{lineno} - {name} - {message}',
-    style='{'
-)
-
-    logger = logging.getLogger(__name__)
-
     encode(args.input_file, args.output_file, args.output_json_file, args.shift)
+
+
+if __name__ == "__main__":
+    main()
+#python encoder.py task_1\origin.txt task_1\encode.txt task_1\key.json 1
---
+++ lab_1/utils/frequency.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+import sys
+
+from collections import Counter
+
+
+project_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_directory)
+
+from utils.r_wr_funcs import read_txt, save_txt
+
+
+def frequency(input_file: str, output_file: str) -> None:
+    """
+    Count frequency for each letter in input file
+
+    Args:
+    input_file: the source text file
+    outpute_file: ready-made frequency analysis
+    """
+    s = read_txt(input_file)
+
+    c = Counter(s)
+    l = len(s)
+    
+    res = ""
+    for key, val in c.most_common():
+        res += f"{key}: {round(val/l, 5)}\n"
+    save_txt(output_file, res)
+    
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description = frequency.__doc__)
+    parser.add_argument("input_file", type = str, help = "Input file name")
+    parser.add_argument("output_file", type = str, help = "Output file name")
+
+    args = parser.parse_args()
+    
+    frequency(args.input_file, args.output_file)
+
+
+if __name__ == "__main__":
+    main()
+    #python utils\frequency.py task_2\origin.txt task_2\frequency.txt
